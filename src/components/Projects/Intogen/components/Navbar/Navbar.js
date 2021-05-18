4d16312f9792bb523ecf17798bc969b817873877
claKue <clarissa.kuemhof@stud.uni-due.de>
2021-05-18 17:32:38 +0200

    nationality in eigenen folder gemacht
    jetzt könnt ihr in navbar euren code reinschreiben, um zu sehen, ob es funktioniert

--- src/components/Projects/Intogen/components/Navbar/Navbar.js
+++ src/components/Projects/Intogen/components/Navbar/Navbar.js
@@ -5,14 +5,6 @@ import MenuItems from './MenuItems';
 import './Navbar.css';
 import logo from './logo.png';
 
-
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-
 const Navbar = () => {
 
     const [click, setClick] = useState()
@@ -26,7 +18,7 @@ const Navbar = () => {
             
 
 
-            {/* <nav>
+             {/* <nav>
                 <ul>
                     <li><NavLink to={'/'}> HOME </NavLink></li>
                     <li> VISUALIZATION
@@ -38,14 +30,7 @@ const Navbar = () => {
                     <li><NavLink to={'/FindYourType'}> FIND YOUR TYPE </NavLink></li>
                     <li><NavLink to={'/About'}> ABOUT US </NavLink></li>
                 </ul>
-            </nav> */}
-
-
-
-
-
-
-
+            </nav>  */}
 
             <div className="nav-center container">
                 <img className="logo" src={logo} alt="Logo" />
@@ -62,78 +47,16 @@ const Navbar = () => {
 
                 {/* hinter click muss eigentlich: <FaTimes/>: <FaBars/> (Probleme beim installieren) */}
             </div>
+
+
+
+            
             <div className="separator">
-                <span>Nationality</span>
+                <span>title</span>
             </div> 
-
-            <div className="chooseNationality">
-                Choose a Nationality: 
-            </div>
-            <div class="col-lg-3">
-                <select id = "countryCmbBox" class="form-control countrypicker" data-live-search="true">
-                </select>
-            </div>
-            {/* <hr class="border"/> */}
-
-
-
-            <div className="cards">
-                <p className="cardsTitle">Kolb's Learning Style</p>
-                <hr className="borderCards"></hr>
-                <Button className="wholeCard">
-                    <Card className="root">
-                        <CardContent>
-                            <Typography className="title" gutterBottom>
-                                ACTIVISTS
-                            </Typography>
-                            <Typography className="subTitle">
-                                (Concrete experiencer/ Active experimenter)
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Button>
-
-                <Button className="wholeCard">
-                    <Card className="root">
-                        <CardContent>
-                            <Typography className="title" gutterBottom>
-                                THEORISTS
-                            </Typography>
-                            <Typography className="subTitle">
-                                (Abstract conceptualiser/Reflective observer)   
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Button>
-
-                <Button className="wholeCard">
-                    <Card className="root">
-                        <CardContent>
-                            <Typography className="title" gutterBottom>
-                                REFLECTORS
-                            </Typography>
-                            <Typography className="subTitle">
-                                (Concrete experiencer/Reflective observer)
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Button>
-
-                <Button className="wholeCard">
-                    <Card className="root">
-                        <CardContent>
-                            <Typography className="title" gutterBottom>
-                                PRAGMATISTS
-                            </Typography>
-                            <Typography className="subTitle">
-                            (Abstract conceptualization/ Active experimenter)
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Button>
-            </div>
         </nav>
     
+            // hier könnt ihr euren Code reintippen, um zu sehen, ob es funktioniert
     ); 
 };
 
