4f58e407c615bcba8125a3ad69ef504a2202f3fe
claKue <clarissa.kuemhof@stud.uni-due.de>
2021-06-06 15:56:26 +0200

    Update Diagram.js

--- src/components/Projects/Intogen/components/diagrams/nationality/Diagram.js
+++ src/components/Projects/Intogen/components/diagrams/nationality/Diagram.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } 
-    from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const data = [
   {
