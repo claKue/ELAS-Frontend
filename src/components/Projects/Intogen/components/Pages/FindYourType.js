e2ff9af5d686ab150d772e0833b99e02cf811b4d
wiwizilli <82824239+wiwizilli@users.noreply.github.com>
2021-07-05 18:45:21 +0200

    deleted conflict

--- src/components/Projects/Intogen/components/Pages/FindYourType.js
+++ src/components/Projects/Intogen/components/Pages/FindYourType.js
@@ -104,14 +104,14 @@ return (
         questions: [
           {
             type: "radiogroup",
-<<<<<<< Updated upstream
+
             name: "introduction",
             title: "Questions are based on David Kolb's research. This questionnaire is designed to define out your preferred learning styles(s) as an adult. Over the years, you have probably developed learning habits that help you benefit more from some experiences than from others. This questionnaire will probably take you about 7-10 minutes to complete. The accuracy of your results depends on how honest you are. There are no right or wrong answers. For each statement, choose if you would rather agree or disagree with it. If you find yourself wondering which situation to think of when answering a question, just think about how you are when you are working with people. Go with your first gut reaction instead of overthinking your response.",
-=======
+
             name: "",
             title: ""
             
->>>>>>> Stashed changes
+
           }
         ]
       }, { 
